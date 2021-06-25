2914f974abcfa24c96d14a2871d77aab018ff6b1
Kevin Sherman <kevin@metadevdigital.com>
2021-06-25 19:27:02 -0400

    Launch Fix

--- CM-Assist/Program.cs
+++ CM-Assist/Program.cs
@@ -13,7 +13,7 @@ namespace CM_Assist
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
